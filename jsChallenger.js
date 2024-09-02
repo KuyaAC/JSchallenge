63c77fd15dd939f256ac48d817d226e49a7f0be1
Allen Carl <aciesnet02@gmail.com>
2024-09-02 21:44:55 +0800

    start new challenge

---
+++ jsChallenger.js
@@ -0,0 +1,23 @@
+//Write a function that takes two numbers (a and b) as argument. Sum a and b. Return the result
+function myFunction(a, b) {
+  return a + b;
+}
+
+//Write a function that takes two values, say a and b, as arguments. Return true if the two values are equal and of the same type
+function myFunction(a, b) {
+  if ((a = b)) {
+    return true;
+  } else {
+    return false;
+  }
+}
+
+//Write a function that takes two values, say a and b, as arguments. Return true if the two values are equal and of the same type
+function myFunction(a, b) {
+  return a === b;
+}
+
+//Write a function that takes a value as argument. Return the type of the value.
+function myFunction(a) {
+  return typeof a;
+}
